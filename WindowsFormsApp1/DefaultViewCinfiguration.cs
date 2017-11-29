e35c62c1025221796a7b4a89b4b35d1dfd49c4e2
Andrey <mopozko123@gmail.com>
2017-11-29 16:10:00 +0500

    Вернул случайно удаленный файл

---
+++ WindowsFormsApp1/DefaultViewCinfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    public class DefaultViewCinfiguration : IViewCinfiguration
+    {
+        public Color Color { get; } = Color.Red;
+        public FontFamily FontFamily { get; } = FontFamily.GenericSerif;
+        public int Width { get; } = 500;
+        public int Height { get; } = 500;
+        public StringFormat StringFormat { get; } = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center,
+            FormatFlags = StringFormatFlags.NoClip
+        };
+    }
+}
